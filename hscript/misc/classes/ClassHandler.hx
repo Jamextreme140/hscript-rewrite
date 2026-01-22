fc77659dc50bf4884fa9048df909b7304c9f0bc7
Jaime Humberto Macias Bustamante <jamextreme140@outlook.com>
2026-01-22 14:27:15 -0700

    Super Class handling (WIP)

--- hscript/misc/classes/ClassHandler.hx
+++ hscript/misc/classes/ClassHandler.hx
@@ -11,16 +11,20 @@ import hscript.Ast.IHScriptCustomBehaviour;
  @:allow(hscript.misc.classes.Instance)
 class ClassHandler implements IHScriptCustomBehaviour {
 
+    private static inline var CLASS_POSFIX:String = "_HSX"; // BIG TODO
+
     public static function createInstance(cl:ClassHandler, args:Array<Dynamic>) {
         return cl.create(args);
     }
 
     public final name:String;
+    public final isFinal:Bool = false; // TODO: final class
     public final classInterp:Interp;
     
     private var module:ScriptRuntime;
     private final clsDecl:ClassDecl;
 	private final constructor:Dynamic;
+    private var inheritance:Null<Dynamic> = null;
 
     public function new(clsDecl:ClassDecl, module:ScriptRuntime) {
         this.module = module;
@@ -28,16 +32,33 @@ class ClassHandler implements IHScriptCustomBehaviour {
         this.classInterp = new ClassInterp(this.name);
         this.clsDecl = clsDecl;
         this.constructor = Reflect.makeVarArgs(function(args) return this.create(args));
+        if(clsDecl.extend != null)
+            createInheritance();
         build();
     }
 
-    private function build() {
+    private function createInheritance() {
+        var extend:String = clsDecl.extend;
+        if(module.variables.exists(extend)) {
+            var cls:ClassHandler = module.variables.get(extend);
+            if(cls.isFinal)
+                throw 'Cannot extend a final class';
+            inheritance = cls;
+        }
+        else
+            inheritance = Type.resolveClass('${extend}$CLASS_POSFIX');
+
+        if(inheritance == null)
+            throw 'Invalid class: ${extend} was not found.';
+    }
+
+    private inline function build() {
         classInterp.errorHandler = module.errorHandler;
         classInterp.publicVariables = module.publicVariables;
         classInterp.execute(clsDecl.body);
     }
 
-    private function create(args:Array<Dynamic>):Dynamic {
+    private function create(args:Array<Dynamic>):Instance {
         return new Instance(args, this);
     }
 
--- hscript/misc/classes/Instance.hx
+++ hscript/misc/classes/Instance.hx
@@ -14,14 +14,24 @@ class Instance implements IHScriptCustomBehaviour {
     private var instanceInterp:InstanceInterp;
     private var classHandler:ClassHandler;
     private var module:ScriptRuntime;
+    private var constructor:Dynamic = null;
+    private var superClass:Dynamic = null;
 
     public function new(args:Array<Dynamic>, classHandler:ClassHandler) {
         this.module = classHandler.module;
         this.classHandler = classHandler;
         build();
 
+        var hasSuperClass = classHandler.inheritance != null;
         if(instanceInterp.variables.exists('new')) {
+            constructor = hasSuperClass ? createConstructor() : null;
             StaticInterp.callObjectField(null, instanceInterp.variables.get('new'), args);
+
+            if(hasSuperClass && this.superClass == null)
+                throw 'super() not called';
+        }
+        else if(hasSuperClass) {
+            createSuperClass(args);
         }
     }
 
@@ -31,6 +41,22 @@ class Instance implements IHScriptCustomBehaviour {
         instanceInterp.publicVariables = module.publicVariables;
         instanceInterp.scriptParent = this;
         instanceInterp.execute(classHandler.clsDecl.body);
+    }
+
+    private inline function createConstructor():Dynamic {
+        return Reflect.makeVarArgs(createSuperClass);
+    }
+
+    private function createSuperClass(args:Array<Dynamic>) {
+        if(classHandler.inheritance is ClassHandler) {
+            var instance:Instance = ClassHandler.createInstance(classHandler.inheritance, args);
+            superClass = instance;
+            // TODO: fetch superclass fields to concatenate with "scriptParentFields"
+        }
+        else {
+            superClass = Type.createInstance(classHandler.inheritance, args);
+            // TODO: fetch superclass fields to concatenate with "scriptParentFields"
+        }
     }
 
     public function hget(field:String):Dynamic {
